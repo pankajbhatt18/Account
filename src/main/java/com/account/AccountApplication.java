bc4ac3977f682a53753ffb255ee41c948a25df66
Pankaj Bhatt <pankajb717gmail.comgit config --global user.email pankajb717@gmail.com>
2020-05-11 20:19:25 +0530

    [Account]Updated fix for tomcat server.

--- src/main/java/com/account/AccountApplication.java
+++ src/main/java/com/account/AccountApplication.java
@@ -3,6 +3,7 @@ package com.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -18,7 +19,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 @EnableJpaRepositories(basePackages = {"com.demo"})
 @EntityScan(basePackages = {"com.demo.entity","com.demo.model"})
 @OpenAPIDefinition(info = @Info(title ="Account/Login MS",version = "1.0.0",description = "Account/Login for performing account/login related queries"  ))
-public class AccountApplication{
+public class AccountApplication extends SpringBootServletInitializer {
 
 		/**
 		 * The main method.
