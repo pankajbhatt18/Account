d42931463b586fe75b867395fb3da181dde59a29
Pankaj Bhatt <pankajb717gmail.comgit config --global user.email pankajb717@gmail.com>
2020-05-12 11:23:43 +0530

    [Account]Added tomcat dependency.

--- src/main/java/com/account/AccountApplication.java
+++ src/main/java/com/account/AccountApplication.java
@@ -3,6 +3,7 @@ package com.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,24 +11,27 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class AccountApplication.
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.demo","com.demo.controller","com.demo.servlet"})
-@EnableJpaRepositories(basePackages = {"com.demo"})
-@EntityScan(basePackages = {"com.demo.entity","com.demo.model"})
-@OpenAPIDefinition(info = @Info(title ="Account/Login MS",version = "1.0.0",description = "Account/Login for performing account/login related queries"  ))
+@ComponentScan(basePackages = { "com.demo", "com.demo.controller", "com.demo.servlet" })
+@EnableJpaRepositories(basePackages = { "com.demo" })
+@EntityScan(basePackages = { "com.demo.entity", "com.demo.model" })
+@OpenAPIDefinition(info = @Info(title = "Account/Login MS", version = "1.0.0", description = "Account/Login for performing account/login related queries"))
 public class AccountApplication extends SpringBootServletInitializer {
 
-		/**
-		 * The main method.
-		 *
-		 * @param args the arguments
-		 */
-		public static void main(String[] args) {
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AccountApplication.class);
+	}
 }
